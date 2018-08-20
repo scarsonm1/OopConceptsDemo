0c5b2147fbfba2265dcb798e81e775791a59278b
Shir <healthylegacy01@gmail.com>
2018-08-20 13:47:07 -0500

    LOC

--- OopConptsDemo/Program.cs
+++ OopConptsDemo/Program.cs
@@ -4,9 +4,37 @@ namespace OopConptsDemo
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+         public static void Main(string[] args)
+        { 
+            // Create 
+            UserInputName UserInputName = newUserInput.Name;
+            UserBookName UserBookName  = newBook.Name;
+            GenreName GenreName = newGenre.Name;
+            TimeItTook TimeItTook = newTimeItTook;
+            TodaysDate TodaysDate = newTodaysDate;
+
+
+            Console.WriteLine("What is your Name?");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your favorite book?");
+            Console.ReadLine();
+
+            Console.WriteLine("What genre is this book?);
+            Console.ReadLine();
+
+            Console.WriteLine("How long did it take you to read this book?")
+            Console.ReadLine();
+
+            Console.WriteLine("What is today's Date?");
+            Console.ReadLine();
+
+            // Update
+
+
+
+
+
         }
     }
 }
---
+++ OopConptsDemo/Romance.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopConptsDemo
+{
+    class Romance
+    {
+        //Create
+        public string ConnectionString { get; private set; }
+
+        public EmployeeRepository(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        // Create
+        public void Insert(employee Employee)
+        {
+            MySqlConnection conn = new MySqlConnection(ConnectionString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+
+                cmd.CommandText = "INSERT INTO products (Name, Price) " +
+                                  "VALUES (@name, @price);";
+                cmd.Parameters.AddWithValue("name", product.Name);
+                cmd.Parameters.AddWithValue("price", product.Price);
+
+                cmd.ExecuteNonQuery();
+
+            }
+}
